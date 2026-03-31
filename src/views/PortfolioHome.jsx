8347ddd7b437cffacbecc37e5f13f289d4b8dc82
josealarconchacon <jose.alarcon.chacon@gmail.com>
2026-03-30 23:43:10 -0400

    update home

--- src/views/PortfolioHome.jsx
+++ src/views/PortfolioHome.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import HeroSection from "../components/portfolio/HeroSection";
 import FloatingBottomNav from "../components/portfolio/FloatingBottomNav";
+import SlidePanelShell from "../components/portfolio/SlidePanelShell";
 
 function PortfolioHome() {
+  const [activePanel, setActivePanel] = useState(null);
+  const panelOpen = activePanel !== null;
+
   return (
-    <div className="flex h-dvh max-h-dvh flex-col overflow-hidden bg-portfolio-bg">
-      <main id="main" className="relative min-h-0 flex-1">
-        <HeroSection />
-      </main>
-      <FloatingBottomNav />
+    <div className="relative h-dvh max-h-dvh overflow-hidden bg-portfolio-bg">
+      <div
+        className={`relative z-0 flex h-full flex-col transition-[transform] duration-300 ease-out will-change-transform ${
+          panelOpen ? "-translate-x-[min(18%,5.5rem)] md:-translate-x-[min(22%,8rem)]" : ""
+        }`}
+      >
+        <main id="main" className="relative min-h-0 flex-1">
+          <HeroSection />
+        </main>
+        <FloatingBottomNav
+          activePanel={activePanel}
+          onOpenPanel={setActivePanel}
+          elevated={panelOpen}
+        />
+      </div>
+
+      <SlidePanelShell
+        open={panelOpen}
+        panelId={activePanel}
+        onClose={() => setActivePanel(null)}
+      />
     </div>
   );
 }
